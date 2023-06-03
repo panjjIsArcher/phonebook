817e17866456696229653e5d443af862988c98b5
panjiajie <251907882@qq.com>
2023-06-03 11:37:39 +0800

    组件引入

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Phonebook />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import Phonebook from "./components/phoneBook.vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Phonebook }
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/phoneBook.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>通讯录</div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue";
+export default defineComponent({
+    
+});
+</script>
